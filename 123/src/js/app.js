c355aa55b845677729d309089b76b7725a2b3b95
vloienko <loenko99@gmail.com>
2022-12-10 00:57:35 +0200

    tesssst

---
+++ 123/src/js/app.js
@@ -0,0 +1,15 @@
+// import * as flsFunctions from './modules/functions.js';
+
+// flsFunctions.isWebp();
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+// const swiper = new Swiper();
+
+// --------------------------------------------------------------
+
+// import './modules/slick.js';
+// import './modules/mixitup.js';
+// import './modules/jquery.fancybox.js';
+
+// --------------------------------------------------------------
